da4cc51dc1ee10bf929baee82f3048583b8b8172
Andrew James <ajames20@gmail.com>
2019-10-20 20:44:30 -0500

    Add Table functions

---
+++ public/Table.js
@@ -0,0 +1,61 @@
+import { html } from "https://unpkg.com/htm/preact/standalone.mjs";
+
+const TableHead = ({ elem, classNames, children }) => html`
+  <${elem} class=${classNames}>
+    ${children}
+  </${elem}>
+`;
+
+const TableHeadCell = ({ classNames, input }) => html`
+  <th class=${classNames}>${input}</th>
+`;
+
+const TableBody = ({ classNames, children }) => html`
+  <tr class=${classNames}>
+    ${children}
+  </tr>
+`;
+
+const TableRow = ({ classNames, children }) => html`
+  <tr class=${classNames}>
+    ${children}
+  </tr>
+`;
+
+const TableData = ({ classNames, input }) => html`
+  <td class=${classNames}>${input}</td>
+`;
+
+const Table = ({ meals = [] }) => html`
+  <table class="table-fixed">
+    <${TableHead}>
+      <${TableRow}>
+        <${TableHeadCell} classNames="w-1/2 px-4 py-2" input="Meal" />
+        <${TableHeadCell} classNames="w-1/2 px-4 py-2" input="Calories" />
+      </${TableRow}>
+    </${TableHead}>
+    <${TableBody}>
+      ${meals.map(
+        meal => html`
+          <${TableRow} classNames="bg-gray-100">
+            <${TableData} classNames="border px-4 py-2" input=${meal.description} />
+            <${TableData} classNames="border px-4 py-2" input=${meal.calories} />
+          </${TableRow}>
+        `
+      )}
+      <${TableRow} classNames="bg-gray-100">
+        <${TableData} classNames="border px-4 py-2" input="Total" />
+        <${TableData}
+          classNames="border px-4 py-2 font-extrabold"
+          input=${totalCalories(meals.map(meal => meal.calories))}
+        />
+      </${TableRow}>
+    </${TableBody}>
+  </table>
+`;
+
+function totalCalories(meals) {
+  return meals.reduce((acc, cals) => acc + cals, 0);
+}
+
+export { Table };
