dd4047454f3acc44ed515da5915e197daa08132d
Andrew James <ajames20@gmail.com>
2019-10-20 20:42:44 -0500

    Add data

---
+++ public/data.js
@@ -0,0 +1,16 @@
+const meals = [
+  {
+    description: "Breakfast",
+    calories: 409
+  },
+  {
+    description: "Lunch",
+    calories: 293
+  },
+  {
+    description: "Dinner",
+    calories: 404
+  }
+];
+
+export { meals };
