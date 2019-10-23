74618bacd5edc6bc64c6d225172f3d994c26c1d7
Andrew James <ajames20@gmail.com>
2019-10-22 21:12:38 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,8 @@
-import hh from "hyperscript-helpers";
-import { h, diff, patch, create } from "virtual-dom";
-
-import initModel from "model";
-// import "../css/styles.css";
+import initModel from "./model";
+import view from "./view";
+import update from "./update";
+import app from "./app";
 
 const root = document.getElementById("app");
-function app(initModel, update, view, node) {
-  let model = initModel;
-  let currentView = view(dispatch, model);
-  let rootNode = create(currentView);
-  node.appendChild(rootNode);
 
-  function dispatch(msg) {
-    model = update(msg, model);
-    const updatedView = view(dispatch, model);
-    const patches = diff(currentView, updatedView);
-    rootNode = patch(rootNode, patches);
-    currentView = updatedView;
-  }
-}
-
-// app(initModel, /* update, view*/ root);
+app(initModel, update, view, root);
