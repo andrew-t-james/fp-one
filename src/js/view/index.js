058b8e1b7972f1a925319e22d5817f309bc2a8d1
Andrew James <ajames20@gmail.com>
2019-10-28 20:12:02 -0500

    Add fieldSet function

--- src/js/view/index.js
+++ src/js/view/index.js
@@ -1,5 +1,10 @@
 import hh from "hyperscript-helpers";
 import { h } from "virtual-dom";
+function fieldSet(labelText, inputValue) {
+  return div({ className: "mb-4" }, [
+    label({ className: classNames.formLabel }, labelText),
+    input({ className: classNames.formInput, type: "text", value: inputValue })
+  ]);
 
 const { pre } = hh(h);
 
