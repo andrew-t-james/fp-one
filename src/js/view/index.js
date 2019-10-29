9ee050ad030a968ac36611abf0602a7aa5226275
Andrew James <ajames20@gmail.com>
2019-10-28 21:04:56 -0500

    Add update meal and description oninput

--- src/js/view/index.js
+++ src/js/view/index.js
@@ -1,39 +1,58 @@
 import hh from "hyperscript-helpers";
 import { h } from "virtual-dom";
 import { classNames } from "./classNames";
-import { MSGS } from "../update";
+import { showFormMsg, mealInputMessage, caloriesInputMessage } from "../update";
 
 const { pre, div, h1, button, form, label, input } = hh(h);
 
-function fieldSet(labelText, inputValue) {
+function fieldSet(labelText, inputValue, oninput) {
   return div({ className: "mb-4" }, [
     label({ className: classNames.formLabel }, labelText),
-    input({ className: classNames.formInput, type: "text", value: inputValue })
+    input({
+      className: classNames.formInput,
+      type: "text",
+      value: inputValue,
+      name: labelText.toLowerCase(),
+      oninput
+    })
   ]);
 }
 
 function buttonSet(dispatch) {
   return div({ className: "flex items-center justify-between" }, [
     button({ className: classNames.buttonPrimary }, "Add Meal"),
-    button({ className: classNames.buttonSecondary }, "Cancel")
+    button(
+      {
+        className: classNames.buttonSecondary,
+        onclick: () => dispatch(showFormMsg(false))
+      },
+      "Cancel"
+    )
   ]);
 }
 
 function formView(dispatch, model) {
   const { description, calories, showForm } = model;
   if (showForm) {
-    return form({ className: classNames.form }, [
-      fieldSet("Meal", description),
-      fieldSet("Calories", calories || ""),
-      buttonSet()
-    ]);
+    return form(
+      { className: classNames.form, onsubmit: e => e.preventDefault() },
+      [
+        fieldSet("Meal", description, e =>
+          dispatch(mealInputMessage(e.target.value))
+        ),
+        fieldSet("Calories", calories || "", e =>
+          dispatch(caloriesInputMessage(e.target.value))
+        ),
+        buttonSet(dispatch)
+      ]
+    );
   }
 
   return div({ className: "mb-4" }, [
     button(
       {
         className: classNames.buttonPrimary,
-        onclick: () => dispatch(MSGS.SHOW_FORM)
+        onclick: () => dispatch(showFormMsg(true))
       },
       "Show Form"
     )
