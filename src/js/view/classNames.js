a55c296cc1c1a64f9712f73236475446c5dd6d70
Andrew James <ajames20@gmail.com>
2019-10-28 20:11:43 -0500

    Add classNames for view

---
+++ src/js/view/classNames.js
@@ -0,0 +1,12 @@
+export const classNames = {
+  container: "container mx-auto",
+  h1: "font-sans text-lg text-gray-800 text-center text-5xl",
+  buttonPrimary:
+    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
+  buttonSecondary:
+    "bg-white hover:bg-gray-100 text-gray-800 font-semibold text-red-500 py-2 px-4 border border-gray-400 rounded shadow",
+  form: "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4",
+  formLabel: "block text-gray-700 text-sm font-bold mb-2",
+  formInput:
+    "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+};
