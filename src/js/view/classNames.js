a7d6c791580b1fc0f669cc329555acee4191e6f1
Andrew James <ajames20@gmail.com>
2019-11-03 17:04:50 -0600

    Add tabelCell classNames

--- src/js/view/classNames.js
+++ src/js/view/classNames.js
@@ -12,5 +12,6 @@ export const classNames = {
   form: "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4",
   formLabel: "block text-gray-700 text-sm font-bold mb-2",
   formInput:
-    "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+  tableCell: "border px-4 py-2"
 };
